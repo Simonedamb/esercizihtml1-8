365cb28024e8a68ab5cddf1a73979c83b2d2b3f8
simone d'ambrosio <simonedamb@hotmail>
2022-03-15 12:04:08 +0100

    exercises_js_2

--- JavaScript/js5/exercise-2.js
+++ JavaScript/js5/exercise-2.js
@@ -3,34 +3,35 @@
 // cto => 1800
 // developer => 1500
 // default => 1000
-const ceoSalary = calculateSalary('ceo');
-const managerSalary = calculateSalary('manager');
-const ctoSalary = calculateSalary('cto');
-const developerSalary = calculateSalary('developer');
-const otherSalary = calculateSalary('other');
 function calculateSalary(role) {
-  switch(role) {
+  switch(role) { 
     case 'ceo':
-      console.log(2200);
-      break;
+    return 2200;
+
     case 'manager':
-      console.log(1800);
-      break;
     case 'cto':
-      console.log(1800);
-      break;
+      return 1800;
+
     case 'developer':
-      console.log(1500);
-      break;
-    case 'other':
-      console.log(1000);
-      break;
+      return 1500;
+
+      default:
+      return 1000;
   }
   
+    
+    
+    
 }
-
-console.log(ceoSalary);
-console.log(managerSalary);
-console.log(ctoSalary);
-console.log(developerSalary);
-console.log(otherSalary);
+  
+  const ceoSalary = calculateSalary('ceo');
+  const managerSalary = calculateSalary('manager');
+  const ctoSalary = calculateSalary('cto');
+  const developerSalary = calculateSalary('developer');
+  const otherSalary = calculateSalary('other');
+  
+  console.log(ceoSalary);
+  console.log(managerSalary);
+  console.log(ctoSalary);
+  console.log(developerSalary);
+  console.log(otherSalary);
