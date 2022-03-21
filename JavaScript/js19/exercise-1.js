b7f12b093b727bca3f86808e0d8a547eadb92f0e
simone d'ambrosio <simonedamb@hotmail>
2022-03-21 15:33:33 +0100

    exercises_javascript_

--- JavaScript/js19/exercise-1.js
+++ JavaScript/js19/exercise-1.js
@@ -9,5 +9,6 @@ person2.firstName = `simon`;
 
 // Modifica la proprietà "firstName" di person2 in "Simon"
 
+// Gli oggetti in Javascript sono molto flessibili, in questo caso quello che viene fatto è passare il riferimento alla locazione di memoria. Quindi sia la variabile person1 che la variabile person2, punteranno allo stesso spazio di memoria.
 console.log(person1);
 console.log(person2);
