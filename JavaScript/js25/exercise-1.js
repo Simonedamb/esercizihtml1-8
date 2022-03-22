e2ddef39aca3fb1aa82f2fc7f1cb3c8702ba7443
simone d'ambrosio <simonedamb@hotmail>
2022-03-22 11:18:49 +0100

    exercise-25

--- JavaScript/js25/exercise-1.js
+++ JavaScript/js25/exercise-1.js
@@ -1,4 +1,28 @@
-const person = {};
+const person = {
+  get firstName() {
+    return this.name;
+  },
+  set firstName(second) {
+    this.name = second;
+  },
+  get lastName() {
+    return this.last;
+  },
+  set lastName(second) {
+    this.last = second;
+  },
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+let john = Object.create(person);
+let simon = Object.create(person);
+
+john.firstName = "John";
+john.lastName = "Doe";
+simon.firstName = "Simon";
+simon.lastName = "Collins";
 
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
