b4a5381ceb44f9a407e2e32a2a926b6a5cc9c325
simone d'ambrosio <simonedamb@hotmail>
2022-03-24 11:44:41 +0100

    £exercise-js39

---
+++ JavaScript/39 - Le classi/exercise-6.js
@@ -0,0 +1,40 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(image) {
+    if (image instanceof Square) {
+      return image.side * image.side;
+    }
+    if (image instanceof Rectangle) {
+      return image.width * image.height;
+    }
+    if (image instanceof Circle) {
+      return image.radius * image.radius;
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
